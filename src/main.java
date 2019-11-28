152db1dea41dd9722f94220d0d0f7292a6874fd8
vitaliy kotov <v.kotov@corp.badoo.com>
2019-11-28 12:40:48 +0300

    remove Singleton

--- src/main.java
+++ src/main.java
@@ -3,9 +3,6 @@ public class main
     public static void main(String[] args) {
         System.out.println("Начало программы");
 
-        Singleton.five();
 
-        System.out.println("Тут мы добрались до объекта");
-        Singleton S = Singleton.getInstance();
     }
 }
