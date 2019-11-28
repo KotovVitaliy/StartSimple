bd25c730fc85a477f8c36a3ac75e319084c6de57
vitaliy kotov <v.kotov@corp.badoo.com>
2019-11-28 12:45:18 +0300

    Factories: step 1

---
+++ src/TestTypes/Test.java
@@ -0,0 +1,13 @@
+package TestTypes;
+
+public class Test {
+    private String type = "undefined";
+    private String speed = "undefined";
+    private String amount = "undefined";
+
+    public void showParams() {
+        System.out.println("Type: " + this.type);
+        System.out.println("Speed: " + this.speed);
+        System.out.println("Amount: " + this.amount);
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,8 +1,11 @@
+import TestTypes.Test;
+
 public class main
 {
     public static void main(String[] args) {
         System.out.println("Начало программы");
 
-
+        Test test = new Test();
+        test.showParams();
     }
 }
